d433ce2443d7ec7f525e33c945c75b0c1994b2aa
David Newcomb <david.newcomb@bigsoft.co.uk>
2024-12-18 17:23:27 +0000

    Updated to show BrowserContext.close bug

--- src/main/java/uk/co/bigsoft/playright_bug_onclose/Main.java
+++ src/main/java/uk/co/bigsoft/playright_bug_onclose/Main.java
@@ -10,13 +10,8 @@ import com.microsoft.playwright.Playwright;
 public class Main {
 
 	public static void main(String[] args) {
-		mainWithBrowserContext(args);
-		mainWithoutBrowserContext(args);
-	}
 
-	public static void mainWithBrowserContext(String[] args) {
-
-		log("Test With BrowserContext");
+		log("Test showing BrowserContext.close() is not called when the browser disapears.");
 		log("");
 
 		Playwright playwright = Playwright.create();
@@ -26,81 +21,38 @@ public class Main {
 		BrowserType bt = playwright.chromium();
 
 		Browser browser = bt.launch(lo);
-		BrowserContext browserContext = browser.newContext();
-		Page page = browserContext.newPage();
+		browser.onDisconnected(it -> { //
+			log("***************************** Browser onDisconnected: " + it); //
+		});
 
+		BrowserContext browserContext = browser.newContext();
 		browserContext.onClose(it -> { //
 			log("***************************** BrowserContext onDisconnected: " + it); //
 		});
 		browserContext.onPage(it -> { //
 			log("***************************** BrowserContext onPage: " + it); //
 		});
-		browser.onDisconnected(it -> { //
-			log("***************************** Browser onDisconnected: " + it); //
-		});
+
+		Page page = browserContext.newPage();
 		page.onClose(it -> { //
 			log("***************************** Page onClose: " + it); //
-		});
-		page.onCrash(it -> { //
-			log("***************************** Page onCrash: " + it); //
-		});
-		page.onPageError(it -> { //
-			log("***************************** Page onPageError: " + it); //
 		});
 
 		for (int i = 10; i != 0; --i) {
 			try {
-				log("Close window, you have " + i + " seconds to comply! Closed=" + page.isClosed());
+				log("Close window, you have " + i + " seconds to comply! Connected/Closed=" + browser.isConnected()
+						+ "/" + page.isClosed());
 				Thread.sleep(1_000);
 			} catch (InterruptedException e) {
 				log("Interrupted!");
 			}
 		}
-		log("Calling page.close()");
-//		page.close();
+		log("I am now calling browserContext.close()");
 		browserContext.close();
+
 		log("Close Playright");
 		playwright.close();
-		log("End");
 
-	}
-
-	public static void mainWithoutBrowserContext(String[] args) {
-
-		log("Test With BrowserContext");
-		log("");
-
-		Playwright playwright = Playwright.create();
-		LaunchOptions lo = new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50);
-		BrowserType bt = playwright.firefox();
-		Browser browser = bt.launch(lo);
-		Page page = browser.newPage();
-
-		browser.onDisconnected(it -> { //
-			log("***************************** Browser onDisconnected: " + it); //
-		});
-		page.onClose(it -> { //
-			log("***************************** Page onClose: " + it); //
-		});
-		page.onCrash(it -> { //
-			log("***************************** Page onCrash: " + it); //
-		});
-		page.onPageError(it -> { //
-			log("***************************** Page onPageError: " + it); //
-		});
-
-		for (int i = 10; i != 0; --i) {
-			try {
-				log("Close window, you have " + i + " seconds to comply! Closed=" + page.isClosed());
-				Thread.sleep(1_000);
-			} catch (InterruptedException e) {
-				log("Interrupted!");
-			}
-		}
-		log("Calling page.close()");
-		page.close();
-		log("Close Playright");
-		playwright.close();
 		log("End");
 
 	}
